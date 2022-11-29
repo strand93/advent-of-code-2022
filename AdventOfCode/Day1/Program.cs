90e4f81519c0dd7b7487db7215c1cd6246f16e90
strand93 <strand.93@hotmail.com>
2022-11-29 22:12:05 +0100

    Set up advent of code template

---
+++ AdventOfCode/Day1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Day1;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var input = File.ReadLines(@"..\..\..\AdventCode_1_input1.txt").ToList();
+        Console.WriteLine(Test());
+        Console.ReadLine(); 
+    }
+
+    private static int Test()
+    {
+        return 1;
+    }
+}
